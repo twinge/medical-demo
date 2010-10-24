d93f6e4590869402b05451905f2f608b288b54ee
Josh Starcher <josh.starcher@gmail.com>
2010-10-24 09:01:06 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medical::Application.config.secret_token = '33d42aa6fff72b8b1473f336b4a902765446de202fd0798800e2f83f7379d9e2e9fc1ee9b2cc5830c3eaaa3690c468cdcf796489e44141a64e77ac14bc1ddb7a'
