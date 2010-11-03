e75deaa3d0add8b935fa0f9dbffd62679ea05cd2
Josh Starcher <josh.starcher@gmail.com>
2010-11-03 17:21:00 -0400

    include channel migrations

--- db/migrate/20101024145124_create_channels.rb
+++ db/migrate/20101024145124_create_channels.rb
@@ -8,6 +8,11 @@ class CreateChannels < ActiveRecord::Migration
 
       t.timestamps
     end
+    
+    Channel.create(:number => 100, :title => 'OR-10: Endo Cam', :callsign => 'haiDVI-hi', :source_url => 'uuid:0001737c-0000-0000-0000-000000000000')
+    Channel.create(:number => 102, :title => 'OR-11: Room Cam', :callsign => 'haiSDI-hi', :source_url => 'uuid:0001737e-0000-0000-0000-000000000000')
+    Channel.create(:number => 101, :title => 'OR-10: Endo Cam', :callsign => 'haiDVI-low', :source_url => 'uuid:0001737d-0000-0000-0000-000000000000')
+    Channel.create(:number => 103, :title => 'OR-11: Room Cam', :callsign => 'haiSDI-low', :source_url => 'uuid:0001737f-0000-0000-0000-000000000000')
   end
 
   def self.down
